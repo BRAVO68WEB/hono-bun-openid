9811d56d0628d96ce9fda6841f05b14142f082d3
bravo68web <jbandyopadhayaya@gmail.com>
2023-09-22 01:55:36 +0530

    lol: pnpm + esbuild it is then

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
 
 import routes from './routers'
 
@@ -9,8 +11,11 @@ app.use('*', logger())
 
 app.route('/', routes)
 
-// app.use('*', serveStatic({
-//     path: './public/404.html',
-// }))
+app.use('*', serveStatic({
+    path: './public/404.html',
+}))
 
-export default app
+serve({
+    fetch: app.fetch,
+    port: Number(process.env.PORT),
+})
